1ae1a7cdb1e37dee9238f2854409e9bc322faadf
czx0916 <554784247@qq.com>
2019-11-09 12:16:31 -0800

    week 4

---
+++ src/ArticleList/ArticleList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import ArticleListItem from "./ArticleListItem";
+
+const ArticleList = props => {
+  return (
+    <ul>
+      {props.articles.map(article => (
+        <li key={article.slug}><ArticleListItem article={article} /></li>
+      ))}
+    </ul>
+  );
+};
+
+ArticleList.propTypes = {
+  articles: PropTypes.array.isRequired
+};
+export default ArticleList;
---
+++ src/ArticleList/ArticleListItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ArticleListItem = props => {
+    return (
+        <section>
+            <h2>{props.article.title}</h2>
+            <p>{props.article.shortText}</p>
+            <time dateTime={props.article.pubYear}>{props.article.pubDate}</time>
+            <button onClick={() => alert(`${props.article.slug}`)}>show article slug</button>
+        </section>
+    );
+};
+
+ArticleListItem.propTypes = {
+    articles: PropTypes.array.isRequired
+  };
+export default ArticleListItem;
